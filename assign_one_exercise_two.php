5177f365fff737eb496218674469b44c0489cb1a
Kyle Leigh Espena <jasyle92@gmail.com>
2020-11-26 23:26:48 +0800

    First commit

--- assign_one_exercise_two.php
+++ assign_one_exercise_two.php
@@ -46,7 +46,7 @@
         $num1 = $_GET['num1'];
         $num2 = $_GET['num2'];
         $div = $num1 / $num2;
-
+o
         echo "The quotient of " .$num1. " and " .$num2. " is " .$div;
     }
 ?>
---
+++ class_user.php
@@ -0,0 +1,23 @@
+<?php
+    class User{
+        public $name;
+        public $username;
+        public $email;
+
+        function __construct($name, $username , $email){
+            $this->name = $name;
+            $this->username = $username;
+            $this->email = $email;
+        } 
+        function addFriend(){
+            echo "{$this->name} .Added a friend<br>";
+        }
+        function postStatus(){
+            echo "{$this->name} .Posted a status<br>";
+        }
+    }
+
+    $kyle=new User ("Kyle Leigh Espeña", "Zayn Yap", "jasyle92@gmail.com");
+    $kyle->addFriend();
+    $kyle->postStatus();
+?>
